7d8c1def34e0d94f12415463f9994a37d5920f9c
IgnisTynka <marcia03835@gmail.com>
2022-08-14 16:06:10 +0200

    Second patr of tutorial

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,29 +1,21 @@
-# puts "EventManager Initialized"
+require 'csv'
+puts "EventManager Initialized"
 
-#reading the file:
-# contents = File.read "event_attendees.csv"
-# puts contents
+#Refactoring Clean Zip Codes
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
 
-#reading the file by line:
-# lines = File.readlines "event_attendees.csv"
-# lines.each do |line|
-#     puts line
-# end 
-
-#display the first name
-# lines = File.readlines "event_attendees.csv"
-# lines.each do |line|
-#     columns = line.split(",")
-#     name = columns[2]
-#     puts name
-# end 
-
-
-# skippinf=g the Header Row
-lines = File.readlines "event_attendees.csv"
-lines.each_with_index do |line, index|
-    next if index == 0 
-    columns = line.split(",")
-    name = columns[2]
-    puts name
-end 
+contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+  )
+  
+  contents.each do |row|
+    name = row[:first_name]
+  
+    zipcode = clean_zipcode(row[:zipcode])
+  
+    puts "#{name} #{zipcode}"
+  end
---
+++ lib/step_by_step.rb
@@ -0,0 +1,96 @@
+require 'csv'
+puts "EventManager Initialized"
+
+#Reading the file:
+contents = File.read "event_attendees.csv"
+puts contents
+
+#Reading the file by line:
+lines = File.readlines "event_attendees.csv"
+lines.each do |line|
+    puts line
+end 
+
+#Display the first name
+lines = File.readlines "event_attendees.csv"
+lines.each do |line|
+    columns = line.split(",")
+    name = columns[2]
+    puts name
+end 
+
+# Skipping the Header Row
+lines = File.readlines "event_attendees.csv"
+lines.each_with_index do |line, index|
+    next if index == 0 
+    columns = line.split(",")
+    name = columns[2]
+    puts name
+end 
+
+#Switching over to use the CSV Library
+contents = CSV.open('event_attendees.csv', headers: true)
+contents.each do |row|
+    name = row[2]
+    puts name
+end
+
+#Accessing Columns by their Names
+contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+)
+contents.each do |row|
+    name = row[:first_name]
+    puts name
+end
+
+#Displaying the Zip Codes of All Attendees
+#and
+#Handling Bad and Good Zip Codes and Missing Zip Codes
+contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+)
+
+contents.each do |row|
+    name = row[:first_name]
+    zipcode = row [:zipcode]
+    if zipcode.nil?
+        zipcode = '00000'
+    elsif zipcode.length < 5
+        zipcode = zipcode.rjust(5, '0')
+    elsif zipcode.length > 5
+        zipcode = zipcode[0..4]
+    end
+    puts "#{name} #{zipcode}"
+end
+
+#Moving Clean Zip Codes to a Method
+def clean_zipcode(zipcode)
+    if zipcode.nil?
+        zipcode = '00000'
+    elsif zipcode.length < 5
+        zipcode = zipcode.rjust(5, '0')
+    elsif zipcode.length > 5
+        zipcode = zipcode[0..4]
+    else 
+        zipcode
+    end
+end
+
+contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+  )
+  
+  contents.each do |row|
+    name = row[:first_name]
+  
+    zipcode = clean_zipcode(row[:zipcode])
+  
+    puts "#{name} #{zipcode}"
+  end
