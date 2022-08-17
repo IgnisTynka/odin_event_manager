d9052fc83e3afca5588bea684f769288f34b283c
IgnisTynka <marcia03835@gmail.com>
2022-08-17 19:43:41 +0200

    Added template in html file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,14 +1,34 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-
 puts "EventManager Initialized"
 
-#Showing All Legislators in a Zip Code
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zip)
+    civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+    civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+    begin
+        legislators = civic_info.representative_info_by_address(
+            address: zip,
+            levels: 'country',
+            roles: ['legislatorUpperBody', 'legislatorLowerBody']
+        )
+
+        legislators = legislators.officials
+
+        legislator_name = legislators.map do |legislator|
+            legislator.name
+        end
+        #  legislator_name = legislators.map(&:name) <- this line do the same 
+
+        legislators_string = legislator_name.join(", ")
+    rescue
+        'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    end
 end
 
 contents = CSV.open(
@@ -16,23 +36,41 @@ contents = CSV.open(
     headers: true,
     header_converters: :symbol
   )
-  
+
+template_letter = File.read('from_letter.html')  
+
 contents.each do |row|
     name = row[:first_name]
   
     zipcode = clean_zipcode(row[:zipcode])
 
-    begin
-        legislators = civic_info.representative_info_by_address(
-            address: zipcode,
-            levels: 'country',
-            roles: ['legislatorUpperBody', 'legislatorLowerBody']
-        )
-        legislators = legislators.officials
-    rescue
-        'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-    end
-  
-    puts "#{name} #{zipcode} #{legislators}"
+    legislators = legislators_by_zipcode(zipcode)
+
+    personal_letter = template_letter.gsub('FIRST_NAME', name)
+    personal_letter.gsub!('LEGISLATORS', legislators)
+
+    puts personal_letter
 end
 
+from_letter = %{
+    <html>
+    <head>
+      <meta charset='UTF-8'>
+      <title>Thank You!</title>
+    </head>
+    <body>
+      <h1>Thanks FIRST_NAME!</h1>
+      <p>Thanks for coming to our conference.  We couldn't have done it without you!</p>
+    
+      <p>
+        Political activism is at the heart of any democracy and your voice needs to be heard.
+        Please consider reaching out to your following representatives:
+      </p>
+    
+      <table>
+        <tr><th>Legislators</th></tr>
+        <tr><td>LEGISLATORS</td></tr>
+      </table>
+    </body>
+    </html>
+}
